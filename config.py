71b5e61678f729572202f244e31cb5ec2faccb77
Pranav 'Baba' Minasandra <pminasandra@ab.mpg.de>
2023-11-27 12:09:07 +0100

    Init

---
+++ config.py
@@ -0,0 +1,15 @@
+
+# Pranav Minasandra
+# pminasandra.github.io
+# November 27, 2023
+
+import os
+import os.path
+
+
+#Directories
+PROJECTROOT = open(".cw", "r").read().rstrip()
+DATA = os.path.join(PROJECTROOT, "Data/")
+FIGURES = os.path.join(PROJECTROOT, "Figures/")
+
+formats=['png', 'pdf', 'svg']
