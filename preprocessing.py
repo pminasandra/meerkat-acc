26ff37e2eae755db4e2131720413a4b0aff4a6a0
Pranav 'Baba' Minasandra <pminasandra@ab.mpg.de>
2023-12-07 18:25:49 +0100

    Preprocessing to remove all the useless data

--- preprocessing.py
+++ preprocessing.py
@@ -9,11 +9,13 @@ issues. This is one-time-use code, and must NOT be run again. This is committed
 on GitHub for diagnostic reasons.
 """
 
+import datetime as dt
 import glob
 import os.path
 
 import pandas as pd
 
+import accreading
 import config
 import utilities
 
@@ -41,6 +43,64 @@ def exterminate_extraneous_column():
             csvfile = pd.read_csv(csvfilepath, index_col=[0])
             csvfile.to_csv(csvfilepath, index=False)
 
+
+def delete_useless_data():
+    """
+    A lot of the data come from collars before or after they were deployed.
+    Speaking to Vlad, I have arrived at which data need to be exterminated.
+    Refer project-notes.md
+    """
+    # this function is just a big hack
+    # rewrite it every single time new data is added
+
+    accgen = accreading.load_acc_files(subset_cols=False)
+    for dplment, filename, df in accgen:
+        print(f"processing {filename}")
+        if dplment == 'NQ_2021_1':
+            df = df[df['Timestamp'] < dt.datetime(2021, 8, 17, 20, 0, 0)]
+            print("general data trimming")
+
+            if 'M019' in filename:
+                print("ind-specific data dropping")
+                df = df[df['Timestamp'] < dt.datetime(2021, 8, 13,
+                                                        0, 0, 0)]
+            if 'NQ_VNQM012_SHTB_Axy_009_20210814-20210817' in filename:
+                print("ind-specific data dropping")
+                df = df[df['Timestamp'] > dt.datetime(2021, 8, 13,
+                                                        7, 0, 0)]
+
+        elif dplment == "RW_2021_1":
+            print("general data trimming")
+            df = df[df['Timestamp'] > dt.datetime(2021, 6, 4, 7, 0, 0)]
+
+            if 'VJXM126_SHMB_Axy013' in filename:
+                print("ind-specific data dropping")
+                df = df[df['Timestamp'] > dt.datetime(2021, 6, 5,
+                                                        7, 0, 0)]
+            if 'VMPF026_RST_Axy019' in filename:
+                print("ind-specific data dropping")
+                df = df[df['Timestamp'] > dt.datetime(2021, 6, 9,
+                                                        7, 0, 0)]
+
+        elif dplment == "ZU_2021_1":
+            print("general data trimming")
+            if 'VZUF054_RRRS_Axy003' in filename:
+                print("ind-specific data dropping")
+                df = df[df['Timestamp'] > dt.datetime(2021, 5, 7,
+                                                        7, 0, 0)]
+            if 'VZUM056_LTRT_Axy004' in filename:
+                print("ind-specific data dropping")
+                df = df[df['Timestamp'] > dt.datetime(2021, 5, 16,
+                                                        7, 0, 0)]
+
+        elif dplment == "ZU_2021_2":
+            print("general data trimming")
+            df = df[df['Timestamp'] < dt.datetime(2021, 7, 25, 10, 0, 0)]
+            df = df[df['Timestamp'].dt.date != dt.date(2021, 7, 19)]
+
+        tgtfilename = os.path.join(config.ACC_GPS_DIR, dplment, filename)
+        df.to_csv(filename + ".csv", index=False)
 if __name__ == "__main__":
+    delete_useless_data()
     pass
 #    exterminate_extraneous_column() # already done, don't re-do
