000f123a5c59b78556ebb1dbd985df5968252194
Pranav 'Baba' Minasandra <pminasandra@ab.mpg.de>
2024-01-17 13:53:25 +0100

    Setup complete

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Pranav Minasandra
+# pminasandra.github.io
+# July 26, 2023
+#
+# Usage:
+# curl -sSf https://raw.githubusercontent.com/pminasandra/bout-duration-distributions/master/setup.sh | bash
+
+mkdir meerkat-acc
+cd meerkat-acc
+
+git clone https://github.com/pminasandra/meerkat-acc classifier
+
+mkdir Data
+mkdir Data/ClassifierRelated
+mkdir Data/Features
+mkdir Data/Predictions
+mkdir Data/PredictionsByIndividual
+mkdir PredictionsByIndividualDeploymentwise
+
+mkdir Figures
+
+echo $PWD > classifier/.cw
